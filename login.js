78b7e79224d452349f3b4df294b987b4923c8ee8
DLIT3 <42062863+DLIT3@users.noreply.github.com>
2018-10-19 12:32:03 +0200

    Add files via upload
    Updated login react section

--- login.js
+++ login.js
@@ -1,87 +1,53 @@
-'use strict';
+import React from 'react';
+import {Button, Input,} from 'antd';
+import { gql, graphql} from 'react-apollo';
 
-const e = React.createElement;
-
-  class Login extends React.Component {
-    state = {
-      user_id: '',
-      password: '',
-    }
-
-    onChange = (e) => {
-      this.setState({
-        [e.target.name]: e.target.value,
-      });
-    }
-
-    onSubmit = async () => {
-      const response = await this.props.mutate({
-        variables: this.state,
-      });
-      console.log(response);
-    }
-    constructor(params) {
-      super(params);
-      this.state = { clients: [] };
-    };
-
-    componentDidMount() {
-      const user_id = 'angryfish882';
-      const password = 'wwww';
-
-      const hmac = new jsSHA('SHA-256', 'TEXT');
-      hmac.setHMACKey(password, 'TEXT');
-      hmac.update(user);
-      hmac.update(Date.now().toString(36).substring(0, 4));
-
-      const token = `${hmac.getHMAC('HEX')}%${user}`;
-
-      const api = axios.create({
-        baseURL: 'http://45.77.58.134:8080',
-        headers: { 'Authorization': 'Bearer ' + token }
-      });
-      function writeName() {
-        var userID = document.getElementById('user_id');
-        var password = document.getElementById('password');
-        var formContent = document.getElementById('login')
-
-        userID.innerHTML = statements.html;
-
-        formContent.innerHTML = "";
-
-      }
-
-      const domContainer =
-      document.querySelector('#login');
-      ReactDOM.render(e(Login), domContainer);
-
-      (async () => {
-        const res = await api.get('/clients');
-        const accounts = await api.get(`/accounts/${res.data[0]._id}`)
-        this.setState({clients: res.data});
-        console.log(accounts);
-      })();
-    };
-
-    render() {
-      return (
-        <div>
-          <Input
-            name = 'angryfish882'
-            placeholder='Username'
-            onChange={e => this.onChange(e)}
-            value={this.state.user_id} />
-          <Input
-            name='password'
-            placeholder='Password'
-            type='password'
-            onChange={e => this.onChange(e)}
-            value={this.state.password} />}
-          <br />
-          <Button onclick={() => this.onSubmit()} type="primary">Sign In</Button>
-        </div>
-      )
-    }
+class Login extends React.Component {
+  state = {
+    username: '',
+    password: '',
   }
 
-  ReactDOM.render(<Login />, document.getElementById('render-element'));
+  onChange = (e) => {
+      this.setState({
+        [e.target.name]: e.target.checked,
+      });
+    }
+
+  onsubmit = async () => {
+    const resonse = await this.props.mutate({
+      variables: this.state,
+    });
+    console.log(response);
+  }
+
+  render() {
+    return (
+      <div>
+        <Input
+        name='username'
+        placeholder='Username'
+        onChange={e => this.onChange(e)}
+        value={this.state.username} />
+        <Input
+        name='password'
+        placeholder='Password'
+        onChange={e => this.onChange(e)}
+        value={this.state.password} />
+        <br />
+        <Button onClick={() => this.onSubmit()} type="primary">Sign In</Button>
+      </div>
+    );
+  }
+}
+
+const mutation = gql
+mutation($username: String!, $password: String!) {
+  login(username: $username, password: $password) {
+    token
+    refreshtoken
+  }
+}
+;
+
+export default graphql(mutation)(Register);
